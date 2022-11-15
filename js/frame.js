48f67e078d45a58a6d03b044d62ccebc937e4cd3
rh1967 <rh@DESKTOP-AD38EG4.localdomain>
2022-11-15 13:23:04 +0100

    ferdinand git page

--- js/frame.js
+++ js/frame.js
@@ -3,8 +3,7 @@
         var frameId = "#" + $( click_para ).parents("div.text").attr("id") + " " + click_para.attr("href") ;                    
         var frameDiv = $( frameId ).parent("div.frame-texts");
         var frameTitle = $( frameId ).attr("title") ;
-        //var gitRepo = "https://rh1967.github.io" ; 
-        var gitRepo = "https://kfngoe.github.io/ferdinand-I-page" ; 
+        var gitRepo = "https://rh1967.github.io" ; 
         var iframe = "https://view.officeapps.live.com/op/embed.aspx?src=" + gitRepo + "/data/docx/start/" + frameTitle + ".docx" ;                    
         var frameTag = frameId + " iframe" ;
         
@@ -23,8 +22,7 @@
         var embedId = "#" + $( click_para ).parents("div.text").attr("id") + " " + click_para.attr("href") ;                    
         var embedDiv = $( embedId ).parent("div.frame-texts");
         var embedTitle = $( embedId ).attr("title") ;
-        //var gitRepo = "https://rh1967.github.io" ;
-        var gitRepo = "https://kfngoe.github.io/ferdinand-I-page" ;  
+        var gitRepo = "https://rh1967.github.io" ; 
         var embed = gitRepo + "/data/pdf/start/" + embedTitle + ".pdf" ;
         var embedTag = embedId + " embed" ;        
         
