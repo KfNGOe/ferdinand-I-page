49c5c5e55d14102860875cf14f0a43a2c5b5fffa
rh1967 <rh@DESKTOP-AD38EG4.localdomain>
2022-11-15 15:02:30 +0100

    ferdinand git page

--- js/frame.js
+++ js/frame.js
@@ -3,13 +3,13 @@
         var frameId = "#" + $( click_para ).parents("div.text").attr("id") + " " + click_para.attr("href") ;                    
         var frameDiv = $( frameId ).parent("div.frame-texts");
         var frameTitle = $( frameId ).attr("title") ;
-        var gitRepo = "https://rh1967.github.io" ; 
+        var gitRepo = "https://kfngoe.github.io/ferdinand-I-page" ;
         var iframe = "https://view.officeapps.live.com/op/embed.aspx?src=" + gitRepo + "/data/docx/start/" + frameTitle + ".docx" ;                    
         var frameTag = frameId + " iframe" ;
         
         $( frameTag ).remove();
-        $( frameId ).append("<iframe></iframe>");                    
-        $( frameTag ).attr("class", "frameStyle").attr( "src", iframe ) ;        
+        $( frameId ).append("<iframe></iframe>");
+        $( frameTag ).attr("class", "frameStyle").attr( "src", iframe ) ;
         
         $( "div.icon-texts" ).show() ;
         $( "div.frame-texts" ).hide();
@@ -19,15 +19,15 @@
     }
     
     function embedLoad( click_para ) {
-        var embedId = "#" + $( click_para ).parents("div.text").attr("id") + " " + click_para.attr("href") ;                    
+        var embedId = "#" + $( click_para ).parents("div.text").attr("id") + " " + click_para.attr("href") ;
         var embedDiv = $( embedId ).parent("div.frame-texts");
         var embedTitle = $( embedId ).attr("title") ;
-        var gitRepo = "https://rh1967.github.io" ; 
+        var gitRepo = "https://kfngoe.github.io/ferdinand-I-page" ; 
         var embed = gitRepo + "/data/pdf/start/" + embedTitle + ".pdf" ;
-        var embedTag = embedId + " embed" ;        
+        var embedTag = embedId + " embed" ;
         
         $( embedTag ).remove();
-        $( embedId ).append("<embed></embed>");                    
+        $( embedId ).append("<embed></embed>");
         $( embedTag ).attr("class", "frameStyle").attr( "src", embed ) ;
 
         $( "div.icon-texts" ).show() ;
