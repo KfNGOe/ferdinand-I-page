b33749b82fced3172aa0a234e293d41ed7f4b68a
rh1967 <rh@DESKTOP-AD38EG4.localdomain>
2022-11-30 19:06:09 +0100

    bug fix page url

--- js/frame.js
+++ js/frame.js
@@ -24,7 +24,7 @@
         var embedId = "#" + $( click_para ).parents("div.text").attr("id") + " " + click_para.attr("href") ;                    
         var embedDiv = $( embedId ).parent("div.frame-texts");
         var embedTitle = $( embedId ).attr("title") ;                
-        var gitRepo = $(location).attr("origin");        
+        var gitRepo = $(location).attr("host");        
         var embed = gitRepo + "/data/pdf/start/" + embedTitle + ".pdf" ;
         var embedTag = embedId + " embed" ;        
         
